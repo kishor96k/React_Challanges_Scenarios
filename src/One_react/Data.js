7b0bed26a07c60a4792e23c0bea784dd658d9ec1
kishor96k <chavankishor813@gmail.com>
2024-01-12 09:54:20 +0530

    details of react one

---
+++ src/One_react/Data.js
@@ -0,0 +1,39 @@
+import img1 from './assets/images/img1.jpg';
+import img2 from './assets/images/img2.jpg';
+import img3 from './assets/images/img3.jpg';
+import img4 from './assets/images/img4.jpg';
+
+const Data = [
+    {
+        id: 1,
+        name: 'kishor',
+        age: 12,
+        image: img1
+    },
+    {
+        id: 2,
+        name: 'chavan',
+        age: 22,
+        image: img2
+    },
+    {
+        id: 3,
+        name: 'darshan',
+        age: 32,
+        image: img3
+    },
+    {
+        id: 5,
+        name: 'prartik',
+        age: 12,
+        image: img4
+    },
+    {
+        id: 4,
+        name: 'shinde',
+        age: 72,
+        image: img1
+    },
+];
+
+export default Data;
