71eaab8b1eb26cf96dafcfc7572dd8f4c25810f8
kishor96k <chavankishor813@gmail.com>
2024-02-09 12:28:23 +0530

    changes for the lorem ipsum generator in react

--- src/9_react/Home.jsx
+++ src/9_react/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { LoremIpsum } from 'lorem-ipsum';
 
 const Home = () => {
+    const lorem = new LoremIpsum();
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div>
+                <strong>on page refresh random lorem ipsum generataor</strong>
+            </div>
+            <h1>Lorem Ipsum Generator</h1>
+            <p>{lorem.generateSentences(18)}</p>
+        </>
     );
 };
 
